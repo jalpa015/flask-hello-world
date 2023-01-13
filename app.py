32c07b5aaa1d3082dbc390aa8c2b12de9049afbb
Jalpa Patel <pjalpa015@gmail.com>
2023-01-13 13:43:48 -0500

    Add more route methods

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello %s!' % name
